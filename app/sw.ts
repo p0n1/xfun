d93820d7986c1a68b76e5f141fa8e52b22c8394f
p0n1 <zp@zerok.io>
2025-08-01 12:20:21 +0800

    Add logging to Twitter video matcher in service worker for better debugging

--- app/sw.ts
+++ app/sw.ts
@@ -65,8 +65,13 @@ const customRuntimeCaching = [
   },
   // Twitter videos: CacheFirst with range request support for video playback
   {
-    matcher: ({ url }: { url: URL }) => 
-      url.hostname === 'video.twimg.com',
+    matcher: ({ url }: { url: URL }) => {
+      const isMatch = url.hostname === 'video.twimg.com';
+      if (isMatch) {
+        console.log('Twitter video matcher hit:', url.href);
+      }
+      return isMatch;
+    },
     handler: new CacheFirst({
       cacheName: 'twitter-videos',
       plugins: [
